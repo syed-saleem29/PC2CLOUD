9ad30fd5768e1c816bbd95fc1a59dc82afdfe949
Syed Saleem <syedsaleem.2993@gmail.com>
2026-05-12 21:53:41 +0530

    Force IPv4 DNS lookup for Gmail SMTP on Render

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,10 +1,11 @@
 const nodemailer = require("nodemailer");
+const dns = require("dns");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
-  family: 4,
+  lookup: (hostname, options, callback) => dns.lookup(hostname, { ...options, family: 4 }, callback),
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
